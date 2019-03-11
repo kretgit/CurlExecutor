3bf9137ee96b19f652e8fdadb439ac3be3101b85
mackret <kret86@yandex.ru>
2019-03-11 20:39:57 +0300

    alone commit

---
+++ src/ru/sbrf/ca/jira/CurlExecutor.java
@@ -0,0 +1,42 @@
+package ru.sbrf.ca.jira;
+
+import org.apache.commons.io.IOUtils;
+
+
+
+public class CurlExecutor {
+
+	public static void main(String[] args) {
+
+		
+		Runtime runtime = Runtime.getRuntime();
+		
+		try {
+			
+			//Process process = runtime.exec("curl -D- -u kretovaa:kretindaatc -X GET -H \"Content-Type: application/json\" "
+					//+ "https://jira.at-consulting.ru/rest/api/2/search?jql=project%20%3D%20OU");
+			
+			Process process = runtime.exec("ifconfig");
+			
+			
+			
+			int resultCode = process.waitFor();
+			
+			
+			if (resultCode == 0) {
+				System.out.println("is: " + IOUtils.toString(process.getInputStream()));
+
+
+			} else {
+				System.out.println(process);
+
+			}
+		} catch (Throwable cause) {
+			//System.out.println("es: " + IOUtils.toString(process.getErrorStream()));
+		}
+		
+		
+		
+	}
+
+}
